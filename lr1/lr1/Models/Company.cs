41f79287dc71c6b26a874bfd561431863c7e06ac
AlexandraBalinskaiya <alexadnrabalinskaiya@gmail.com>
2024-09-15 19:06:24 +0300

    lr1

---
+++ lr1/lr1/Models/Company.cs
@@ -0,0 +1,21 @@
+﻿namespace lr1.Models
+{
+    namespace lr1.Models
+    {
+        public class Company
+        {
+            public string Name { get; set; } = string.Empty; // Назва компанії
+            public string Industry { get; set; } = string.Empty; // Індустрія, в якій працює компанія
+            public int NumberOfEmployees { get; set; } = 0; // Кількість співробітників
+            public decimal Revenue { get; set; } = 0m; // Річний дохід компанії
+            public string Headquarters { get; set; } = string.Empty; // Головний офіс компанії
+
+            public override string ToString()
+            {
+                return $"Company: {Name}, Industry: {Industry}, Employees: {NumberOfEmployees}, Revenue: ${Revenue}M, HQ: {Headquarters}";
+            }
+        }
+
+    }
+
+}
---
+++ lr1/lr1/Program.cs
@@ -0,0 +1,25 @@
+using lr1.Models.lr1.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using lr1.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+var myCompany = new Company
+{
+    Name = "TechCorp",
+    Industry = "Technology",
+    NumberOfEmployees = 5000,
+    Revenue = 120.5m,
+    Headquarters = "Silicon Valley"
+};
+
+app.MapGet("/", () => myCompany.ToString());
+// Генерація випадкового числа від 1 до 100
+//app.MapGet("/", () => new Random().Next(1, 101));
+
+
+
+app.Run();
