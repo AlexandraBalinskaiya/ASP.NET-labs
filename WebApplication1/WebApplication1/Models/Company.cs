bf92e5d7dee43501161e5663344b0ae02235d43a
AlexandraBalinskaiya <alexadnrabalinskaiya@gmail.com>
2024-09-15 18:52:19 +0300

    lr1

---
+++ WebApplication1/WebApplication1/Models/Company.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Models
+{
+public class Company
+{
+    public string Name { get; set; } = string.Empty; // Назва компанії
+    public string Industry { get; set; } = string.Empty; // Індустрія, в якій працює компанія
+    public int NumberOfEmployees { get; set; } = 0; // Кількість співробітників
+    public decimal Revenue { get; set; } = 0m; // Річний дохід компанії
+    public string Headquarters { get; set; } = string.Empty; // Головний офіс компанії
+
+    public override string ToString()
+    {
+        return $"Company: {Name}, Industry: {Industry}, Employees: {NumberOfEmployees}, Revenue: ${Revenue}M, HQ: {Headquarters}";
+    }
+}
+
+}
