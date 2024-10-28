ddcd1f538c77789b304b81050e4a0206200058d0
AlexandraBalinskaiya <alexadnrabalinskaiya@gmail.com>
2024-10-28 14:57:23 +0200

    lr7

---
+++ lr7/lr7/Controllers/FileController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace lr7.Controllers
+{
+    public class FileController : Controller
+    {
+        // Відображення форми
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        // Обробка введених даних і створення файлу
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            // Формуємо вміст файлу
+            string content = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+
+            // Визначаємо шлях файлу
+            string filePath = $"{fileName}.txt";
+
+            // Конвертуємо вміст у байти
+            byte[] fileBytes = Encoding.UTF8.GetBytes(content);
+
+            // Повертаємо файл на завантаження
+            return File(fileBytes, "text/plain", filePath);
+        }
+    }
+}
---
+++ lr7/lr7/Views/File/DownloadFile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lr7.Views.Files
+{
+    public class DownloadFileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
