b0202a2f5a1a1f5adc34392b977c08b80004ea10
AlexandraBalinskaiya <alexadnrabalinskaiya@gmail.com>
2024-11-04 09:33:45 +0200

    lr8

---
+++ lr8/lr8/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lr8.Models;
+
+namespace lr8.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { ID = 1, Name = "Product A", Price = 50.5m, CreatedDate = DateTime.Now.AddDays(-10) },
+                new Product { ID = 2, Name = "Product B", Price = 75.0m, CreatedDate = DateTime.Now.AddDays(-20) },
+                new Product { ID = 3, Name = "Product C", Price = 100.0m, CreatedDate = DateTime.Now.AddDays(-5) }
+            };
+
+            return View(products);
+        }
+    }
+}
---
+++ lr8/lr8/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace lr8.Models
+{
+    public class Product
+    {
+        public int ID { get; set; } // Унікальний ідентифікатор продукту
+        public string Name { get; set; } // Назва продукту
+        public decimal Price { get; set; } // Ціна продукту
+        public DateTime CreatedDate { get; set; } // Дата створення продукту
+    }
+}
